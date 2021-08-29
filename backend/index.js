c5537222eb5ea60cfab0db97ce90c0aa38348bb3
nakul <nakul@qburst.com>
2021-08-29 12:10:04 +0530

    express server initialisation

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
+const express = require('express')
 const mongo = require('./mongo')
+
+const app = express();
+const port = process.env.PORT || 8080
 
 const connectToMongoDB = async () => {
     await mongo().then(mongoose => {
@@ -11,4 +15,5 @@ const connectToMongoDB = async () => {
     })
 }
 
+app.listen(port, console.log('Server started at', port))
 connectToMongoDB();
